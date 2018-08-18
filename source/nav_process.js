9210c4e9d9e6c51f2af30e0abab2ad5e91ecefe8
vmiis <zzm.vmiis@gmail.com>
2018-08-18 20:21:22 +1000

    update

--- source/nav_process.js
+++ source/nav_process.js
@@ -24,7 +24,7 @@ $vm.nav_click_process=function(name,slot){
 //-----------------------------------------
 $vm.nav_load_module=function(name,slot,config){
 	//var input=config;
-	if($vm.app_config.modules!=undefined && $vm.app_config.modules[name]!=undefined && $vm.app_config.modules[name].url!=undefined){
+	if($vm.app_config!=undefined && $vm.app_config.modules!=undefined && $vm.app_config.modules[name]!=undefined && $vm.app_config.modules[name].url!=undefined){
 		if($vm.module_list[name]==undefined){
 			$vm.module_list[name]=$vm.app_config.modules[name];
 		}
